36501a7f896eee8fb5cc46b65cf9bdf95e3361f4
“leonshimizu” <lmshimizu@gmail.com>
2021-11-23 17:10:18 -0800

    Init + add mapbox + hide api key

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <div id='map' style='width: 400px; height: 300px;'></div>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+  import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
+  export default {
+    data: function () {
+      return {
+        message: "Welcome to Vue.js!",
+      };
+    },
+    mounted: function () {
+      this.generateMap();
+    },
+    methods: {
+      generateMap: function() {
+          mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_KEY;
+        const map = new mapboxgl.Map({
+            container: 'map', // container ID
+            style: 'mapbox://styles/mapbox/streets-v11', // style URL
+            center: [-74.5, 40], // starting position [lng, lat]
+            zoom: 9 // starting zoom
+        });
+      }
+    },
+  };
+</script>
