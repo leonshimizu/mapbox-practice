7911769126190fd140df3f58cbbc1e1e60d7c6f7
“leonshimizu” <lmshimizu@gmail.com>
2021-11-23 17:22:53 -0800

    added default markers

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -25,9 +25,18 @@
         const map = new mapboxgl.Map({
             container: 'map', // container ID
             style: 'mapbox://styles/mapbox/streets-v11', // style URL
-            center: [-74.5, 40], // starting position [lng, lat]
+            center: [12.5, 55.7], // starting position [lng, lat]
             zoom: 9 // starting zoom
         });
+        const marker1 = new mapboxgl.Marker()
+          .setLngLat([12.554729, 55.70651])
+          .addTo(map);
+          
+          // Create a default Marker, colored black, rotated 45 degrees.
+          const marker2 = new mapboxgl.Marker({ color: 'black', rotation: 45 })
+          .setLngLat([12.65147, 55.608166])
+          .addTo(map);
+
       }
     },
   };
