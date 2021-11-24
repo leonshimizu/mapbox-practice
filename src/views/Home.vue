ced3df9f4dd247d19d0c75bb08b3bfaf95add273
“leonshimizu” <lmshimizu@gmail.com>
2021-11-23 18:36:43 -0800

    added animate map camera around a point

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,8 @@
     <div id='map-2' style='width: 400px; height: 300px;'></div>
     <br>
     <div id='map-3' style='width: 400px; height: 300px;'></div>
+    <br>
+    <div id='map-4' style='width: 400px; height: 300px;'></div>
   </div>
 </template>
 
@@ -43,7 +45,8 @@
     mounted: function () {
       this.generateMap();
       this.generateMap2();
-      this.generateMap3()
+      this.generateMap3();
+      this.generateMap4();
     },
     methods: {
       generateMap: function() {
@@ -176,7 +179,73 @@
         map.getSource('video').pause();
         }
         });
-      }
+      },
+      generateMap4: function() {
+        mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_KEY;
+        const map = new mapboxgl.Map({
+        container: 'map-4',
+        style: 'mapbox://styles/mapbox/light-v10',
+        center: [-87.62712, 41.89033],
+        zoom: 15.5,
+        pitch: 45
+        });
+        
+        function rotateCamera(timestamp) {
+        // clamp the rotation between 0 -360 degrees
+        // Divide timestamp by 100 to slow rotation to ~10 degrees / sec
+        map.rotateTo((timestamp / 100) % 360, { duration: 0 });
+        // Request the next frame of the animation.
+        requestAnimationFrame(rotateCamera);
+        }
+        
+        map.on('load', () => {
+        // Start the animation.
+        rotateCamera(0);
+        
+        // Add 3d buildings and remove label layers to enhance the map
+        const layers = map.getStyle().layers;
+        for (const layer of layers) {
+        if (layer.type === 'symbol' && layer.layout['text-field']) {
+        // remove text labels
+        map.removeLayer(layer.id);
+        }
+        }
+        
+        map.addLayer({
+        'id': '3d-buildings',
+        'source': 'composite',
+        'source-layer': 'building',
+        'filter': ['==', 'extrude', 'true'],
+        'type': 'fill-extrusion',
+        'minzoom': 15,
+        'paint': {
+        'fill-extrusion-color': '#aaa',
+        
+        // use an 'interpolate' expression to add a smooth transition effect to the
+        // buildings as the user zooms in
+        'fill-extrusion-height': [
+        'interpolate',
+        ['linear'],
+        ['zoom'],
+        15,
+        0,
+        15.05,
+        ['get', 'height']
+        ],
+        'fill-extrusion-base': [
+        'interpolate',
+        ['linear'],
+        ['zoom'],
+        15,
+        0,
+        15.05,
+        ['get', 'min_height']
+        ],
+        'fill-extrusion-opacity': 0.6
+        }
+        });
+        });
+      },
     },
   };
 </script>
